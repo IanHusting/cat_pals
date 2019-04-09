b088bace51c6b64dfa643623be7c426bbc2294e2
ian.husting <ihusting@eye-t.lu>
2019-04-09 16:07:41 +0200

    Updates

---
+++ precache-manifest.9a749d977be19ad356e90168392d6985.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/cat_pals/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "5c36365823f624dcd6f6",
+    "url": "/cat_pals/static/js/runtime~main.5c363658.js"
+  },
+  {
+    "revision": "b80bce79523bee3da63b",
+    "url": "/cat_pals/static/js/main.b80bce79.chunk.js"
+  },
+  {
+    "revision": "63c7be1e6d0808c52c52",
+    "url": "/cat_pals/static/js/2.63c7be1e.chunk.js"
+  },
+  {
+    "revision": "b80bce79523bee3da63b",
+    "url": "/cat_pals/static/css/main.924ced75.chunk.css"
+  },
+  {
+    "revision": "63c7be1e6d0808c52c52",
+    "url": "/cat_pals/static/css/2.87b7582a.chunk.css"
+  },
+  {
+    "revision": "0da75642f88d2b54a553a4fb8f78cb1b",
+    "url": "/cat_pals/index.html"
+  }
+];
